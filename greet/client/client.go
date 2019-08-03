38d0ebb74f005b3a0b556550e8f565c12eea36df
stkhr <tshinichi0813@gmail.com>
2019-08-03 12:27:13 +0900

    feat: add greet stream server and client

--- greet/client/client.go
+++ greet/client/client.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"github.com/stkhr/study-go-gRPC/greet/greetpb"
 	"google.golang.org/grpc"
+	"io"
 	"log"
 )
 
@@ -19,6 +20,8 @@ func main() {
 	c := greetpb.NewGreetServiceClient(cc)
 
 	doUnary(c)
+
+	doServerStreaming(c)
 }
 
 func doUnary(c greetpb.GreetServiceClient) {
@@ -34,4 +37,32 @@ func doUnary(c greetpb.GreetServiceClient) {
 		log.Fatalf("[ERROR] error with calling Greeting: %v", err)
 	}
 	log.Printf("[INFO] response: %v", res.Result)
+}
+
+func doServerStreaming(c greetpb.GreetServiceClient) {
+	fmt.Println("[INFO] starting server streaming client")
+
+	req := &greetpb.GreetManyTimesRequest{
+		Greeting: &greetpb.Greeting{
+			FirstName: "test2",
+			LastName:  "hoge2",
+		},
+	}
+	res, err := c.GreetManyTimes(context.Background(), req)
+	if err != nil {
+		log.Fatalf("[ERROR] error with calling GreetManyTimes: %v", err)
+	}
+
+	for {
+		msg, err := res.Recv()
+		if err == io.EOF {
+			// reached the end of stream
+			break
+		} else if err != nil {
+			log.Fatalf("[ERROR] error with reading stream: %v", err)
+		} else {
+			log.Printf("[INFO] response: %v", msg.GetResult())
+		}
+	}
+
 }
